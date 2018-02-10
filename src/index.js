7deb100311731913d9ccd6ad3e5c66d146862ff4
daiyuqing <765804005@qq.com>
2018-02-10 15:33:37 +0800

    1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// import './index.css';
+import fetchJsonp from 'fetch-jsonp';
+
 class App extends React.Component{
 	constructor(){
 		super();
 		this.state={
-			a:'一路向北...'
+			a:'一路向北'
 		}
 	}
+	componentWillMount(){
+		// fetch('http://localhost:2000/getRank').then((res)=>{
+		// 	return res.json();
+		// }).then((data)=>{
+		// 	console.log(data);
+		// });
+		// fetch('http://m.kugou.com/singer/class&json=true', {
+  //           method : 'GET',
+  //           mode : 'no-cors',
+  //           credentials: 'include'
+  //       }).then((res)=>{
+  //       	res.json();
+  //       }).then(function(responseText){
+  //           console.log(responseText);
+  //       }, function(error){
+  //           console.log(error);
+  //       });
+		// fetchJsonp('http://m.kugou.com/singer/class&json=true',{jsonpCallback: 'callback'}).then((res)=>{
+		// 	res.json();
+		// }).then((data)=>{
+		// 	console.log(data);
+		// });
+	}
 	render(){
-		return (<div>
+		return (<div className='a'>
 			<p>{this.state.a}</p>
+			<div>asfadasd</div>
 			<audio  autoPlay="autoplay">
 			  <source src="http://video.5054399.com/music/touwenzid/ylxb.mp3" type="audio/mpeg" />
 			Your browser does not support the audio element.
--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,58 @@
-var path=require('path');
-var webpack=require('webpack');
-var HtmlWebpackPlugin=require('html-webpack-plugin');
-var ExtractTextPlugin=require('extract-text-webpack-plugin');
-const OpenBrowserPlugin = require('open-browser-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
+const OpenBrowserPlugin = require('open-browser-webpack-plugin');//打开浏览器
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // 自动生成build文件夹及文件：
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
+const ROOT_PATH = path.resolve(__dirname);
+const SRC_PATH = path.resolve(ROOT_PATH, 'src');
+const BUILD_PATH = path.resolve(ROOT_PATH, 'build');
 
-module.exports={
-	devtool: 'cheap-module-source-map',
-	entry:{
-		index:['./src/index.js']
-	},
-	output:{
-		path:path.resolve(__dirname,'build'),
-		filename:'[name].js'
-	},
-	module: {
-	  	loaders: [
-	  	  	{
-	  	  	  	test: /\.js$/,
-	  	  	  	exclude: /node_modules/,
-	  	  	  	loader: 'babel-loader',
-	  	  	  	query: {
-	  	  	  	  	 presets: ['react', 'es2015']
-	  	  	  	}
-	  	  	},
-	  	  	{
-	  	  	  	test: /\.css$/,
-	  	  	  	exclude: /node_modules/,
-	  	  	  	loader: [ 'style-loader', 'css-loader' ]
-	  	  	}
-	  	]
-	},
-	plugins:[
-		new webpack.HotModuleReplacementPlugin(),
-		new webpack.NamedModulesPlugin(),
-		new webpack.optimize.UglifyJsPlugin(),
-		new ExtractTextPlugin('style.css'),
-		new HtmlWebpackPlugin({
-			title:'index',
-			filename:'index.html',
-			template: './public/index.html',
-			inject:'body',
-			favicon:'./src/favicon.ico',
-			// hash:true,
-			cache:true,
-			chunks:['index']
-		}),
-		new OpenBrowserPlugin({url: 'http://localhost:3000'})
-	]
-}
+module.exports = {
+    devtool: 'eval-source-map',
+    entry: {
+        index: [path.resolve(SRC_PATH, 'index.js')]
+    },
+    output: {
+        path: BUILD_PATH,
+        filename: 'js/[name].js',
+        publicPath: '/'
+    },
+    resolve: {
+        extensions: [".js", ".json", ".jsx", ".css", ".scss"],
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: path.resolve(SRC_PATH, 'node_modules'),
+            include: SRC_PATH,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['react', 'es2015', 'stage-0']
+                }
+            }
+        }, {
+            test: /\.css$/,
+            use: {
+            	loader:['css-loader','style-loader']
+            }
+        }]
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({minimize: true}),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('development')
+        }),
+        new HtmlWebpackPlugin({
+            title: 'react-music',
+            favicon: './src/favicon.ico',
+            template: './public/index.html',
+            filename: 'index.html',
+            inject: 'body'
+        }),
+        new ExtractTextPlugin("./src/index.css"),
+        new OpenBrowserPlugin({url: 'http://localhost:3000'})
+    ]
+};
