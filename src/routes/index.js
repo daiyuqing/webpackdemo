03be7a23cecc177e6ce415619ce3f2e888f22c04
daiyuqing <765804005@qq.com>
2018-08-04 22:59:42 +0800

    HashRouter路由改为BrowserRouter

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,15 @@
-import { HashRouter, Route, hashHistory, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, hashHistory, Switch } from 'react-router-dom';
 import Home from 'components/home.js';
 import Project from 'components/project.js';
 import Skill from 'components/skill.js';
 import React, {Component} from 'react';
 const routes=()=>{
-	return <HashRouter history={hashHistory}>
+	return <BrowserRouter history={hashHistory}>
 			<Switch>
 			  <Route exact path='/' component={Home}/>
 			  <Route path='/project' exact component={Project}/>
 			  <Route path='/skill' exact component={Skill}/>
 			</Switch>
-		</HashRouter>
+		</BrowserRouter>
 }
 export default routes;
